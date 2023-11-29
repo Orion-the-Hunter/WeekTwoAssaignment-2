8995c2c101f537bdfa621180f8ae0fe8302885b5
Orion-the-Hunter <sofor0099@gmail.com>
2023-11-29 18:25:30 +0600

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// data fetching function -->
 let categoryCode;
 const loadData = (Code) => {
     categoryCode=Code;
@@ -20,11 +21,13 @@ const loadData = (Code) => {
 
 loadData(1000);
 
-const displayData=(data)=>{
+// displaying function-->
+const displayData = (data) => {
     console.log(data);
-    const videoContainer=document.getElementById("video-section");
-    videoContainer.innerHTML="";
-    if(data.length === 0) {
+    const videoContainer = document.getElementById("video-section");
+    videoContainer.innerHTML = "";
+
+    if (data.length === 0) {
         const errorCard = document.createElement('div');
         errorCard.classList.add('error', 'text-center', 'mt-5', 'pt-5');
         errorCard.innerHTML = `
@@ -33,29 +36,43 @@ const displayData=(data)=>{
         `;
         videoContainer.appendChild(errorCard);
         return "";
-    };
-    data.forEach((videos)=> {
-        const card=document.createElement("div");
-        
+    }
+
+    data.forEach((videos) => {
+        const card = document.createElement("div");
         card.className = "vcards";
-        card.innerHTML=`
-        <img class="vImg" src=${videos.thumbnail} alt="thumbPic">
-        <div class="channel">
-            <img class="channel-img" src=${videos.authors[0].profile_picture} alt="channelPic">
-            <h4>${videos.title}</h4>
-        </div>
-        <div class=" verify">
-            <h6>${videos.authors[0].profile_name}</h6>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
-            <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
-            </svg>
-        </div>
-        <p>${videos.others.views}</p>
-        `;
+
+        if (videos.authors[0].verified === true) {
+            card.innerHTML = `
+                <img class="vImg" src=${videos.thumbnail} alt="thumbPic">
+                <div class="channel">
+                    <img class="channel-img" src=${videos.authors[0].profile_picture} alt="channelPic">
+                    <h4>${videos.title}</h4>
+                </div>
+                <div class="verify">
+                    <h6>${videos.authors[0].profile_name}</h6>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+                        <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+                    </svg>
+                </div>
+                <p>${videos.others.views}</p>
+            `;
+        } else {
+            card.innerHTML = `
+                <img class="vImg" src=${videos.thumbnail} alt="thumbPic">
+                <div class="channel">
+                    <img class="channel-img" src=${videos.authors[0].profile_picture} alt="channelPic">
+                    <h4>${videos.title}</h4>
+                </div>
+                <h6>${videos.authors[0].profile_name}</h6>
+                <p>${videos.others.views}</p>
+            `;
+        }
         videoContainer.appendChild(card);
     });
 };
 
+// view sorting function -->
 const sortByViews = () => {
     console.log(categoryCode);
     fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryCode}`)
